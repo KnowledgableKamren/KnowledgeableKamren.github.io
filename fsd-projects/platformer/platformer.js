827106300f2685441c57fc0521bfd8eb42c6d4da
KnowledgableKamren <kamren.woods@walterlcohen.org>
2026-04-17 16:29:49 +0000

    worked on <platformer.js>

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -29,6 +29,14 @@ createCollectable("diamond", 1350, 410, 0.5, 0.7);
 createPlatform(300, 610, 600, 50, "red");
 createPlatform(1000, 510, 500, 60, "red");
 createCannon("right", 650, 1000);
+createPlatform(950, 270, 4400, 60, "red");
+createPlatform(0, 410, 300, 60, "red");
+createPlatform(520, 330, 400, 60, "red");
+createCollectable("diamond", 1200, 210, 0.5, 0.7);
+createCannon("top", 650, 1500);
+createCannon("top", 950, 1500);
+createCannon("left", 655, 1000);
+createCollectable("diamond", 710, 210, 0.5, 0.7);
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
